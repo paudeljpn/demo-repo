a10e70b6ea193b0ea6e198d5bbd3d012c7c60d39
Dambar Raj Paudel <paudeljpn@gmail.com>
2023-01-25 22:44:33 +0900

    cc file added

---
+++ cc.sh
@@ -0,0 +1,5 @@
+ffplay -vf scale=720:-1 https://d3rkqpxeqi5qol.cloudfront.net/livedarts/darts.stream/playlist.m3u8 &
+ffplay -vf scale=720:-1 https://d3rkqpxeqi5qol.cloudfront.net/livedarts/darts.stream/playlist.m3u8 &
+ffplay -vf scale=720:-1 https://d3rkqpxeqi5qol.cloudfront.net/livedarts/darts.stream/playlist.m3u8 &
+ffplay -vf scale=720:-1 https://d3rkqpxeqi5qol.cloudfront.net/livedarts/darts.stream/playlist.m3u8
+t
